2044a2f6ec18c25c54bd106ecfe539cc7b76e8b3
Antonioangy <dumitruangy26@gmail.com>
2022-01-27 14:39:40 +0100

    esercizio

---
+++ database/migrations/2022_01_25_174027_create_songs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->string('genres', 60);
+            $table->date('publication_date');
+            $table->string('author', 60);
+            $table->string('record_company', 100);
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
---
+++ database/migrations/2022_01_26_152527_create_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',60);
+            $table->integer('n_songs');
+            $table->integer('sold');
+            $table->string('record_company', 100);
+            $table->string('genres', 60);
+            $table->date('publication_date');
+            $table->string('author', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
