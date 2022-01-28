af406956949fb3a2f5446ce4cd366c4eed5eddaa
Antonioangy <dumitruangy26@gmail.com>
2022-01-28 17:32:20 +0100

    es finito

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            AlbumSeeder::class,
+            ArtistSeeder::class,
+            SongSeeder::class
+        ]);
     }
 }
